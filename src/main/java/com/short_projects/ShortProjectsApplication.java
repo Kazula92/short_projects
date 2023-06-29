4b109228f7e2f3f5c2cceb71ac83d31f2bef7a61
Krzysztof.Kazula <kkazula92@gmail.com>
2023-06-29 15:55:24 +0200

    First commit

---
+++ src/main/java/com/short_projects/ShortProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.short_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/short_projects/ShortProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.short_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
